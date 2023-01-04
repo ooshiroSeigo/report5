db2eedddfbb31010218fc0c42bacf8a77fc8f23e
oshiro seigo <e225761@ie.u-ryukyu.ac.jp>
2023-01-04 15:55:50 +0900

    finished step2

--- src/Error.java
+++ src/Error.java
@@ -1,6 +1,12 @@
 public class Error {
     public static void main(String[] args) throws Exception {
         String str = null;
+        try{
         System.out.println(str.length());
+        }
+        catch(NullPointerException exception){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(exception.getMessage());
+        }
     }
 }
