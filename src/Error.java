2c0636cce8bcccd05ddc08d8ee0fef107d36b4e4
oshiro seigo <e225761@ie.u-ryukyu.ac.jp>
2023-01-04 16:03:43 +0900

    finished step3

--- src/Error.java
+++ src/Error.java
@@ -1,12 +1,9 @@
+import java.util.StringTokenizer;
+
 public class Error {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try{
-        System.out.println(str.length());
-        }
-        catch(NullPointerException exception){
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(exception.getMessage());
-        }
+        String str = "壱百万";
+        int value = Integer.parseInt(str);
+
     }
 }
